b11cbab6d337cebd89eeac67d59898b6065e9e8b
Fernando Herrero <alphp@users.noreply.github.com>
2019-07-09 20:57:34 +0200

    Fix namespaces

--- src/SimpleSVGDocument.php
+++ src/SimpleSVGDocument.php
@@ -2,14 +2,14 @@
 /*******************************************************************************
   Simple SVG - Simple PHP class for creating SVG documents
     Author          : Fernando Herrero
-    Version         : 1.0.0
+    Version         : 1.0.1
     License         : MIT
 		Home page       : https://github.com/fawno/SimpleSVG
 *******************************************************************************/
 
 	namespace SimpleSVG;
 
-	use SimpleSVGElement;
+	use SimpleSVG\SimpleSVGElement;
 
 	class SimpleSVGDocument {
 		protected $svg = null;
--- src/SimpleSVGElement.php
+++ src/SimpleSVGElement.php
@@ -2,7 +2,7 @@
 /*******************************************************************************
   Simple SVG - Simple PHP class for creating SVG documents
     Author          : Fernando Herrero
-    Version         : 1.0.0
+    Version         : 1.0.1
     License         : MIT
 		Home page       : https://github.com/fawno/SimpleSVG
 *******************************************************************************/
